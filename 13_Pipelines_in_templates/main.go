723774613e0116aae29fc3224d7a12649de098c5
Hamed Yarandi <hamed.yarandi@gmail.com>
2023-07-23 21:34:40 +0330

    pipelines in templates no2

--- 13_Pipelines_in_templates/main.go
+++ 13_Pipelines_in_templates/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"math"
 	"os"
 	"text/template"
 	"time"
@@ -12,6 +13,8 @@ var tpl *template.Template
 var fm = template.FuncMap{
 	"fdateMDY": monthDayYear,
 	"fdateDMY": dayMonthYear,
+	"fdbl":     double,
+	"fsq":      square,
 }
 
 func monthDayYear(t time.Time) string {
@@ -22,12 +25,20 @@ func dayMonthYear(t time.Time) string {
 	return t.Format("02-01-2006")
 }
 
+func double(x int) int {
+	return x + x
+}
+
+func square(x int) float64 {
+	return math.Pow(float64(x), 2)
+}
+
 func init() {
 	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gohtml"))
 }
 
 func main() {
-	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", time.Now())
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", 3) //time.Now()
 	if err != nil {
 		log.Fatalln(err)
 	}
