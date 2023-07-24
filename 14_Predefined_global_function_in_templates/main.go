2afd0df0f8cc867fdb6fbdcfceb3b578175bb807
Hamed Yarandi <hamed.yarandi@gmail.com>
2023-07-24 14:49:50 +0330

    Using IF AND GT predifined functions in templates

--- 14_Predefined_global_function_in_templates/main.go
+++ 14_Predefined_global_function_in_templates/main.go
@@ -12,13 +12,36 @@ func init() {
 	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
 }
 
+type Words struct {
+	Id     int
+	Name   string
+	Family string
+	Age    int
+}
+
 func main() {
 
-	xs := []string{"zero", "one", "two", "three"}
-	data := struct {
-		Words []string
-		Lname string
-	}{xs, "Hamed"}
+	//xs := []string{"zero", "one", "two", "three"}
+	//data := struct {
+	//	Words []string
+	//	Lname string
+	//}{xs, "Hamed"}
+
+	//defined new struct for checking if and and predefined function in templates
+
+	w1 := Words{
+		Id:     1,
+		Name:   "Hamed",
+		Family: "Yarandi",
+		Age:    35,
+	}
+	w2 := Words{
+		Id:     2,
+		Name:   "Mina",
+		Family: "Fam",
+		Age:    32,
+	}
+	data := []Words{w1, w2}
 
 	err := tpl.Execute(os.Stdout, data)
 	if err != nil {
