cdf9c624570a53ab376ba12100cecc575fe60315
Hamed Yarandi <hamed.yarandi@gmail.com>
2023-07-24 16:07:33 +0330

    Using IF AND GT for scores predifined functions in templates

--- 14_Predefined_global_function_in_templates/main.go
+++ 14_Predefined_global_function_in_templates/main.go
@@ -13,10 +13,12 @@ func init() {
 }
 
 type Words struct {
-	Id     int
-	Name   string
-	Family string
-	Age    int
+	Id          int
+	Name        string
+	Family      string
+	Age         int
+	FirstScore  float64
+	SecondScore float64
 }
 
 func main() {
@@ -30,16 +32,20 @@ func main() {
 	//defined new struct for checking if and and predefined function in templates
 
 	w1 := Words{
-		Id:     1,
-		Name:   "Hamed",
-		Family: "Yarandi",
-		Age:    35,
+		Id:          1,
+		Name:        "Hamed",
+		Family:      "Yarandi",
+		Age:         35,
+		FirstScore:  12.3,
+		SecondScore: 14.2,
 	}
 	w2 := Words{
-		Id:     2,
-		Name:   "Mina",
-		Family: "Fam",
-		Age:    32,
+		Id:          2,
+		Name:        "Mina",
+		Family:      "Fam",
+		Age:         32,
+		FirstScore:  11.8,
+		SecondScore: 8.9,
 	}
 	data := []Words{w1, w2}
 
