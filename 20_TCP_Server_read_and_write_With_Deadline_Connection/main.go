5645b50ac3ce19cfdd51c54b26c0e8694bbe4699
Hamed Yarandi <hamed.yarandi@gmail.com>
2023-07-26 14:29:41 +0330

    TCP Server, read write from connection with deadline

---
+++ 20_TCP_Server_read_and_write_With_Deadline_Connection/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+
+	li, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Println(err)
+	}
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		ln := scanner.Text()
+		fmt.Println(ln)
+		fmt.Fprintf(conn, "I heard you say:%s\n", ln)
+	}
+	defer conn.Close()
+	//we never get here
+	//we have an open stream connection
+	//how dose the above reader know when it's done?
+	fmt.Println("code got here")
+}
