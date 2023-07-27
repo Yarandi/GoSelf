1fa1ec0667879ce2c11e38a8131fc6e29cb19a95
Hamed Yarandi <hamed.yarandi@gmail.com>
2023-07-27 14:26:58 +0330

    retriving from request value and using Makefile tools for automated run command

---
+++ 24_retriving_form_values/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"text/template"
+)
+
+type hotdog int
+
+var tpl *template.Template
+
+func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	tpl.ExecuteTemplate(w, "index.gohtml", r.Form)
+}
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
+}
+
+func main() {
+	var d hotdog
+	http.ListenAndServe(":8080", d)
+}
