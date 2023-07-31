a76a2518e9691f6949992a265973905bc90df52b
Hamed Yarandi <hamed.yarandi@gmail.com>
2023-07-31 17:54:34 +0330

    using http.statusNotFound instead of 404 status code

--- 36_Serving_web_with_http.FileServer/main.go
+++ 36_Serving_web_with_http.FileServer/main.go
@@ -8,12 +8,14 @@ import (
 
 func main() {
 
+	http.HandleFunc("/notfound", notFound)
 	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
 }
 
 func notFound(w http.ResponseWriter, r *http.Request) {
 	_, err := os.Open("tedd.jpg")
 	if err != nil {
-		http.Error(w, "not found", 404)
+		//http.Error(w, "not found", 404)
+		http.Error(w, "not found", http.StatusNotFound)
 	}
 }
