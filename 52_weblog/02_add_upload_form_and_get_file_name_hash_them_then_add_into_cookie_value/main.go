4e2ae7b61f40d6dcbce4cec1e6f603c0ca74105c
Hamed Yarandi <hamed.yarandi@gmail.com>
2023-08-08 11:47:01 +0330

    add hashed image name to cookie then serve on web

--- 52_weblog/02_add_upload_form_and_get_file_name_hash_them_then_add_into_cookie_value/main.go
+++ 52_weblog/02_add_upload_form_and_get_file_name_hash_them_then_add_into_cookie_value/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"crypto/sha1"
+	"fmt"
 	"github.com/google/uuid"
 	"html/template"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -14,7 +19,9 @@ func init() {
 }
 
 func main() {
+
 	http.HandleFunc("/", index)
+	http.Handle("/upload/", http.StripPrefix("/upload", http.FileServer(http.Dir("./upload"))))
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 
@@ -22,7 +29,55 @@ func main() {
 
 func index(w http.ResponseWriter, r *http.Request) {
 	c := getCookie(w, r)
-	c = appendValue(w, c)
+
+	//get the file name and hash them
+	if r.Method == http.MethodPost {
+		f, h, err := r.FormFile("file")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer f.Close()
+		//hash filename
+		hash := sha1.New()
+
+		//get the file name and extension
+		parts := strings.Split(h.Filename, ".")
+		ext := parts[len(parts)-1]
+
+		io.Copy(hash, f)
+		fname := fmt.Sprintf("%x", hash.Sum(nil)) + "." + ext
+		fmt.Println(fname)
+
+		//getting working directory
+		pwd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+		// Create subdirectories if they don't exist
+		err = os.MkdirAll(filepath.Join("upload", "users", "files"), 0755)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		//create new file in upload/users path
+		path := filepath.Join(pwd, "upload", "users", "files", fname)
+		nf, err := os.Create(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		//defer nf.Close()
+
+		//copy
+		// Moves the reading cursor of the uploaded file back to the beginning. This is needed before copying the file content to the newly created file.
+		f.Seek(0, 0)
+		//Copies the content of the uploaded file to the newly created file. This effectively saves the uploaded file with the new hashed filename and extension.
+		io.Copy(nf, f)
+
+		//add file name to users cookie
+		c = appendValue(w, c, fname)
+	}
+
 	xs := strings.Split(c.Value, "|")
 	tpl.ExecuteTemplate(w, "index.gohtml", xs)
 }
@@ -43,26 +98,13 @@ func getCookie(w http.ResponseWriter, r *http.Request) *http.Cookie {
 	return c
 }
 
-func appendValue(w http.ResponseWriter, c *http.Cookie) *http.Cookie {
+func appendValue(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
 
-	//values
-	p1 := "rivers.jpg"
-	p2 := "trees.jpg"
-	p3 := "sands.jpg"
-
-	//append name of the pictures in the session value, if it does not contain the name
 	s := c.Value
-	if !strings.Contains(s, p1) {
-		s += "|" + p1
-	}
-	if !strings.Contains(s, p2) {
-		s += "|" + p2
-	}
-	if !strings.Contains(s, p3) {
-		s += "|" + p3
+	if !strings.Contains(s, fname) {
+		s += "|" + fname
 	}
 	c.Value = s
 	http.SetCookie(w, c)
 	return c
-
 }
