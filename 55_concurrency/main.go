908f9c701493ea8d7613be456d9f4ed9dff008c2
Hamed Yarandi <hamed.yarandi@gmail.com>
2023-08-13 16:55:06 +0330

    concurrency and WaitingGroup

--- 55_concurrency/main.go
+++ 55_concurrency/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
 )
 
@@ -9,10 +10,18 @@ var wg sync.WaitGroup
 
 func main() {
 
+	fmt.Println("OS \t\t", runtime.GOOS)
+	fmt.Println("ARCH \t\t", runtime.GOARCH)
+	fmt.Println("CPUs \t\t", runtime.NumCPU())
+	fmt.Println("Goroutines \t", runtime.NumGoroutine())
+
 	wg.Add(1)
 	go foo()
 
 	bar()
+
+	fmt.Println("CPUs \t\t", runtime.NumCPU())
+	fmt.Println("Goroutines \t", runtime.NumGoroutine())
 	wg.Wait()
 }
 
