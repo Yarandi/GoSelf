172a7f4386c78eb57002484a6f8b2cea1e2b7860
Hamed Yarandi <hamed.yarandi@gmail.com>
2023-08-14 10:36:46 +0330

    doing

--- 55_concurrency/04_using_mutex_to_prevent_race_condition/main.go
+++ 55_concurrency/04_using_mutex_to_prevent_race_condition/main.go
@@ -6,20 +6,24 @@ import (
 	"time"
 )
 
+var wg sync.WaitGroup
 var mu sync.Mutex
 
 func main() {
 
+	defer wg.Wait()
 	var counter int
-	for i := 0; i < 1000; i++ {
 
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
 		go func() {
+			defer wg.Done()
 			mu.Lock()
 			counter++
 			mu.Unlock()
 		}()
-
 	}
+
 	time.Sleep(time.Second)
 	fmt.Println("Counter", counter)
 }
