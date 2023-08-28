bee602ff0e1308b613c85713e6cbd0a9153fd04b
Hamed Yarandi <hamed.yarandi@gmail.com>
2023-08-28 23:42:18 +0330

    adding createUser function

--- 60_MVC/02_json/main.go
+++ 60_MVC/02_json/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"html/template"
+	"log"
 	"net/http"
 )
 
@@ -19,8 +20,13 @@ func main() {
 	router := httprouter.New()
 
 	router.GET("/", index)
-	//add route plus parameters
+
+	//add route for creating user
+	router.POST("/user", createUser)
+
+	//add route plus parameters for getting user
 	router.GET("/user/:id", getUser)
+
 	http.ListenAndServe(":8080", router)
 }
 
@@ -46,4 +52,27 @@ func getUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 
 	// Write JSON data directly to the response
 	w.Write(jsonData)
+}
+
+func createUser(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
+	u := models.User{}
+
+	//decode to save
+	err := json.NewDecoder(req.Body).Decode(&u)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//change Id
+	u.Id = "879054"
+
+	//marshal to show again
+	jsonData, _ := json.Marshal(u)
+
+	//write content type / status code / and Payload
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated) // 201
+
+	fmt.Fprintf(w, "%s \n", jsonData)
 }
