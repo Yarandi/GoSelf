b5e5f3afeff4661119dbd5476dec9bed4bbc5169
Hamed Yarandi <hamed.yarandi@gmail.com>
2023-11-12 13:21:34 +0330

    mongodb crud and change channel example

--- 07_Channel/main.go
+++ 07_Channel/main.go
@@ -1,27 +1,45 @@
+//package main
+//
+//import (
+//	"fmt"
+//	"time"
+//)
+
+//func main() {
+//
+//	intChan := make(chan int)
+//
+//	go CalculateValue(intChan)
+//
+//	result := <- intChan
+//	log.Println(result)
+//}
+//
+//
+//func CalculateValue(intChan chan int){
+//
+//	rand.Seed(time.Now().UnixMicro())
+//	rand := rand.Intn(10009)
+//
+//	intChan <- rand
+//}
+
 package main
 
 import (
-	"log"
-	"math/rand"
+	"fmt"
 	"time"
 )
 
+func printNumbers() {
+	for i := 1; i <= 5; i++ {
+		fmt.Println(i)
+		time.Sleep(1 * time.Second)
+	}
+}
+
 func main() {
-
-	intChan := make(chan int)
-
-	go CalculateValue(intChan)
-
-	result := <- intChan
-	log.Println(result)
+	go printNumbers()
+	time.Sleep(10 * time.Second)
+	fmt.Println("Main function")
 }
-
-
-func CalculateValue(intChan chan int){
-
-	rand.Seed(time.Now().UnixMicro())
-	rand := rand.Intn(10009)
-
-	intChan <- rand
-}
-
